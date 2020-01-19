04558f962d5b310b64331871d2c93beb912bc113
Ching-Yi Hung <brent12052003@gmail.com>
2020-01-19 18:52:44 +0800

    allow pgroonga container to create mutliple databases at once when initializing

---
+++ side_project/postgres_init_files/create-multiple-postgresql-databases.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+set -u
+
+function create_user_and_database() {
+	local database=$(echo $1 | tr ',' ' ' | awk  '{print $1}')
+	local owner=$(echo $1 | tr ',' ' ' | awk  '{print $2}')
+	echo "  Creating user '$owner' and database '$database'"
+    echo "SELECT 'CREATE DATABASE $database' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$database')\gexec" | psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d postgres
+    psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d postgres <<-EOSQL
+    DO \$\$
+        BEGIN
+          CREATE USER $owner;
+          EXCEPTION WHEN OTHERS THEN
+          RAISE NOTICE 'not creating user $owner -- it already exists';
+        END \$\$;
+    GRANT ALL PRIVILEGES ON DATABASE $database TO $owner;
+EOSQL
+}
+
+if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
+	echo "Multiple database creation requested: $POSTGRES_MULTIPLE_DATABASES"
+	for db in $(echo $POSTGRES_MULTIPLE_DATABASES | tr '|' ' '); do
+		create_user_and_database $db
+	done
+	echo "Multiple databases created"
+fi
