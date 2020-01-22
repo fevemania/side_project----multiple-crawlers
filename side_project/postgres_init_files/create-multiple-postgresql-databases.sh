c06319a680bc1fca12e9317cb7b8f7d18600c11a
fevemania <brent12052003@gmail.com>
2020-01-22 10:02:17 +0000

    some progress

--- side_project/postgres_init_files/create-multiple-postgresql-databases.sh
+++ side_project/postgres_init_files/create-multiple-postgresql-databases.sh
@@ -8,7 +8,7 @@ function create_user_and_database() {
     local owner=$(echo $1 | tr ',' ' ' | awk  '{print $2}')
     echo "  Creating user '$owner' and database '$database'"
     echo "SELECT 'CREATE DATABASE $database' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$database')\gexec" | psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d postgres
-    psql -v ON_ERROR_STOP=1 -U "$POSTGRES_USER" -d postgres <<-EOSQL
+    psql -v ON_ERROR_STOP=1 -U "$owner" -d $database <<-EOSQL
     DO \$\$
         BEGIN
           CREATE USER $owner;
